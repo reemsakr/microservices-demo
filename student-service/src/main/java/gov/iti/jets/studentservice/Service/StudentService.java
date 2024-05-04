aadc82575aed7f6a6c54af87081b904149151f16
Reem Sakr <reemsakr060@gmail.com>
2024-05-05 01:06:24 +0300

    updates

--- student-service/src/main/java/gov/iti/jets/studentservice/Service/StudentService.java
+++ student-service/src/main/java/gov/iti/jets/studentservice/Service/StudentService.java
@@ -3,7 +3,6 @@ package gov.iti.jets.studentservice.Service;
 import gov.iti.jets.studentservice.repository.StudentRepository;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Getter
--- student-service/src/main/java/gov/iti/jets/studentservice/entity/Student.java
+++ student-service/src/main/java/gov/iti/jets/studentservice/entity/Student.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.lang.annotation.Documented;
 import java.util.ArrayList;
 import java.util.List;
 
