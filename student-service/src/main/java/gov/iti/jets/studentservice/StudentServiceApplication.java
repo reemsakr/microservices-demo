71b5f2205013cb4ab75d521881d4341b2f1e21b7
Reem Sakr <reemsakr060@gmail.com>
2024-05-09 16:11:19 +0300

    conflict

--- student-service/src/main/java/gov/iti/jets/studentservice/StudentServiceApplication.java
+++ student-service/src/main/java/gov/iti/jets/studentservice/StudentServiceApplication.java
@@ -6,9 +6,11 @@ import gov.iti.jets.studentservice.repository.StudentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
+
 
 @SpringBootApplication
 public class StudentServiceApplication {
--- student-service/src/main/java/gov/iti/jets/studentservice/mappers/StudentMapper.java
+++ student-service/src/main/java/gov/iti/jets/studentservice/mappers/StudentMapper.java
@@ -3,10 +3,8 @@ package gov.iti.jets.studentservice.mappers;
 import gov.iti.jets.studentservice.dto.StudentDTO;
 import gov.iti.jets.studentservice.entity.Student;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-import java.util.List;
 
 @Mapper
 public interface StudentMapper {
